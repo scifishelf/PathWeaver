d7fa460c751d4cb797396c33c1c0c322c6aef178
Ansgar Simon <ansgar@scifi-shelf.de>
2026-03-16 14:54:11 +0100

    chore(01-05): remove dom-to-image-more package and .d.ts stub
    - npm uninstall dom-to-image-more (removed from dependencies)
    - Delete web/src/types/dom-to-image-more.d.ts type stub
    - Build and tests verified passing after removal

--- web/src/types/dom-to-image-more.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'dom-to-image-more' {
-  const api: {
-    toPng(node: HTMLElement, options?: any): Promise<string>
-    toSvg(node: HTMLElement, options?: any): Promise<string>
-  }
-  export default api
-}
-
-
